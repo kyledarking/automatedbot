0175150a7cf9df32377ec791eb451d2f06a2187d
Cliffvincent <143686507+Cliffvincent@users.noreply.github.com>
2024-02-09 15:47:09 +0000

    Sheshh

--- script/help.js
+++ script/help.js
@@ -27,11 +27,11 @@ module.exports.run = async function({
 			let end = start + pages;
 			let helpMessage = `🔴🟢🟡\n𝗖𝗢𝗠𝗠𝗔𝗡𝗗 𝗟𝗜𝗦𝗧:\n\n`;
 			for (let i = start; i < Math.min(end, commands.length); i++) {
-				helpMessage += `╭─╮\n|\t『 ${i + 1}.』  ${prefix}${commands[i]}\n╰────────ꔪ\n`;
+				helpMessage += `╭─╮\n|\t『 ${i + 1}.』  ${prefix}${commands[i]}\n╰───────────ꔪ\n`;
 			}
 			helpMessage += '\n𝗘𝗩𝗘𝗡𝗧 𝗟𝗜𝗦𝗧:\n\n';
 			eventCommands.forEach((eventCommand, index) => {
-				helpMessage += `╭─────────╮\n|\t『 ${index + 1}.』  ${prefix}${eventCommand}\n╰─────────╯ \n`;
+				helpMessage += `╭─────────────────╮\n|\t『 ${index + 1}.』  ${prefix}${eventCommand}\n╰─────────────────╯ \n`;
 			});
 			helpMessage += `\nPage ${page}/${Math.ceil(commands.length / pages)}. To view the next page, type '${prefix}help page number'. To view information about a specific command, type '${prefix}help command name'.`;
 			api.sendMessage(helpMessage, event.threadID, event.messageID);
